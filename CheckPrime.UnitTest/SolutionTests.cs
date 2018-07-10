890c186ef6ff4a0b4f84dca87ab15e0476224362
galaxymaxx <galaxymaxx@outlook.com>
2018-07-09 21:41:53 -0400

    + Optimize logic

---
+++ CheckPrime.UnitTest/SolutionTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Xml.Schema;
+using NUnit.Framework;
+
+namespace CheckPrime.UnitTest
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        private Solution _solution;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _solution = new Solution();            
+        }
+        
+        [Test]
+        [TestCase(-2,false)]
+        [TestCase(2,true)]
+        [TestCase(3,true)]
+        [TestCase(4,false)]
+        [TestCase(5,true)]
+        [TestCase(6,false)]
+        public void IsPrimeMap_whenCalled_ReturnIfItIsAPrime(int num,bool expectedResult)
+        {
+            var result = _solution.IsPrimeMap(num);
+            Assert.That(result,Is.EqualTo(expectedResult));
+        }
+        
+        [Test]
+        [TestCase(-2,false)]
+        [TestCase(2,true)]
+        [TestCase(3,true)]
+        [TestCase(4,false)]
+        [TestCase(5,true)]
+        [TestCase(6,false)]
+        public void IsPrime_whenCalled_ReturnIfItIsAPrime(int num,bool expectedResult)
+        {
+            var result = _solution.IsPrime(num);
+            Assert.That(result,Is.EqualTo(expectedResult));
+        }
+    }
+}
--- CheckPrime/Program.cs
+++ CheckPrime/Program.cs
@@ -11,33 +11,53 @@ namespace CheckPrime
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to using prime checker");
-            Console.Write("Enter a number: ");
-            int userInput = GetUserInput();
-            var solution = new Solution();
-            var result = solution.IsPrimeMap(userInput);
-            Console.WriteLine($"{userInput} is "+(result?"":"not ")+"a prime number");
-            Console.WriteLine("Please any key to quit");
+            do
+            {
+                int testNumber = GetUserInput();
+                CheckIfInputIsPrime(testNumber);
+            } while (DoesUserWantToContinue());
+            
+            Console.WriteLine("Bye");
             Console.ReadKey();
+        }
+
+        private static bool DoesUserWantToContinue()
+        {
+            Console.WriteLine("Do you want to continue? Y/N");
+            var isContinued = Console.ReadKey(true);
+            return isContinued.Key == ConsoleKey.Y;
+        }
+
+        private static void CheckIfInputIsPrime(int userInput)
+        {
+            var solution = new Solution();
+            bool result = solution.IsPrimeMap(userInput);
+            Console.WriteLine($"{userInput} is " + (result ? "" : "not ") + "a prime number");
         }
 
         private static int GetUserInput()
         {
             do
             {
-                var userInput = Console.ReadLine();
-                try
-                {
-                    int num = Convert.ToInt32(userInput);
-                    return num;
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Your input is not a number");
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine("Your number is too big to calculate");
-                }
+                Console.Write("Enter a number: ");
+                string userInput = Console.ReadLine();
+                bool isSucceed = int.TryParse(userInput,out int result);
+                if (isSucceed) return result;
+                Console.WriteLine("Not accepted");
+
+//                try
+//                {
+//                    int num = Convert.ToInt32(userInput);
+//                    return num;
+//                }
+//                catch (FormatException)
+//                {
+//                    Console.WriteLine("Your input is not a number");
+//                }
+//                catch (OverflowException)
+//                {
+//                    Console.WriteLine("Your number is too big to calculate");
+//                }
             } while (true);
         }
     }
